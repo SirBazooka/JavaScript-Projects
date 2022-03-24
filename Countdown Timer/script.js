9d5812ed9c6e00775d7f80d1544e9880510d22dc
SirBazooka <andreimidari@gmail.com>
2022-03-24 19:50:24 +0200

    countdown timer for 10 minutes

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -1,11 +1,19 @@
-let value = 1;
-let time = 10;
+const totalMinutes = 10;
+let time = totalMinutes * 60;
 
+const countDownEl = document.getElementById('countdown');
 
-setInterval(function() {
-    document.getElementById("timer").innerHTML = time;
-    time-= value;
-    document.getElementById("timer").innerHTML = time;
-    if (time == 0) {return;}
-  },1000);
+setInterval(countDownTImer,1000);
+
+function countDownTImer() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds =  seconds < 10 ? '0' + seconds : seconds;
+
+  countDownEl.innerHTML = `${minutes} : ${seconds}`;
+  time--;
+}
+ 
+
 
