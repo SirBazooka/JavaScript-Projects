12cc0416498d265c591848bc9ef878377c12e8ab
SirBazooka <andreimidari@gmail.com>
2022-03-25 21:55:31 +0200

    random color generator

---
+++ Color Flipper/script.js
@@ -0,0 +1,6 @@
+document.getElementById('color').onclick = function changeContent() {
+
+    document.getElementById('color').textContent = "Help me";
+    document.getElementById('color').style = "Color: blue";
+ 
+ }
